d7c9df4fc6bebe8ee699076caa1cb3f64581097e
demanLiu <qq384012351@gmail.com>
2021-04-10 20:28:24 +0800

    update

---
+++ 763.划分字母区间.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+/*
+ * @lc app=leetcode.cn id=763 lang=java
+ *
+ * [763] 划分字母区间
+ *
+ * https://leetcode-cn.com/problems/partition-labels/description/
+ *
+ * algorithms
+ * Medium (76.36%)
+ * Likes:    479
+ * Dislikes: 0
+ * Total Accepted:    62.2K
+ * Total Submissions: 81.6K
+ * Testcase Example:  '"ababcbacadefegdehijhklij"'
+ *
+ * 字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 输入：S = "ababcbacadefegdehijhklij"
+ * 输出：[9,7,8]
+ * 解释：
+ * 划分结果为 "ababcbaca", "defegde", "hijhklij"。
+ * 每个字母最多出现在一个片段中。
+ * 像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * S的长度在[1, 500]之间。
+ * S只包含小写字母 'a' 到 'z' 。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            last[S.charAt(i) - 'a'] = i;
+        }
+        int start = 0, end = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < S.length(); i++) {
+            end = Math.max(end, last[S.charAt(i) - 'a']);
+            if (end == i) {
+                list.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return list;
+    }
+}
+// @lc code=end
